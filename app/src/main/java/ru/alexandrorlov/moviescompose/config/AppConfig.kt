ac0e7857b62c6d79cc0d5f0d75e29551a30b9972
orlovae <79202875363@ya.ru>
2023-03-05 17:42:12 +0300

    init Network

---
+++ app/src/main/java/ru/alexandrorlov/moviescompose/config/AppConfig.kt
@@ -0,0 +1,11 @@
+package ru.alexandrorlov.moviescompose.config
+
+import ru.alexandrorlov.moviescompose.BuildConfig
+
+
+object AppConfig {
+    const val STARS_RATING_COUNT = 4
+    const val ASSETS_FILE_DATA = "data.json"
+    const val TMDB_API_KEY = BuildConfig.TMDB_API_KEY
+    const val ACTOR_COUNT = 6
+}

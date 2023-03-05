038f38e1fa22fb4142763c23271f7d40fe8ca8c6
orlovae <79202875363@ya.ru>
2023-03-05 17:42:12 +0300

    init Network

--- app/src/main/java/ru/alexandrorlov/moviescompose/config/Config.kt
+++
@@ -1,6 +0,0 @@
-package ru.alexandrorlov.moviescompose.config
-
-object Config {
-    const val NUMBER_STARS = 4
-    const val ASSETS_FILE_DATA = "data.json"
-}
--- app/src/main/java/ru/alexandrorlov/moviescompose/util/AssetsReader.kt
+++ app/src/main/java/ru/alexandrorlov/moviescompose/util/AssetsReader.kt
@@ -3,7 +3,7 @@ package ru.alexandrorlov.moviescompose.util
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import ru.alexandrorlov.moviescompose.App
-import ru.alexandrorlov.moviescompose.config.Config
+import ru.alexandrorlov.moviescompose.config.AppConfig
 import ru.alexandrorlov.moviescompose.network.MoviePopularNetwork
 import ru.alexandrorlov.moviescompose.network.MoviesPopularNetwork
 
@@ -14,7 +14,7 @@ object AssetsReader {
 
     private val stringFromJson = App.appContext
         .assets
-        .open(Config.ASSETS_FILE_DATA)
+        .open(AppConfig.ASSETS_FILE_DATA)
         .bufferedReader()
         .use {
             it.readText()

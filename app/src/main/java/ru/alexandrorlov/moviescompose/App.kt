4ec03129295eef4248f3aa19fb64022c034d5ec7
orlovae <79202875363@ya.ru>
2023-03-09 09:52:29 +0300

    refactoring renaming delete comment

--- app/src/main/java/ru/alexandrorlov/moviescompose/App.kt
+++ app/src/main/java/ru/alexandrorlov/moviescompose/App.kt
@@ -4,52 +4,14 @@ import android.app.Application
 import android.content.Context
 
 class App : Application() {
-//    private val repositoryNetworkConfiguration: RepositoryNetworkConfiguration =
-//        RepositoryNetworkConfiguration.Singleton.instance
-//    private val _stateConfigURLImage: MutableStateFlow<StateConfigURLImage> =
-//        MutableStateFlow(StateConfigURLImage.Loading)
-//    private val stateConfigURLImage = _stateConfigURLImage.asStateFlow()
-//    private val appScope = CoroutineScope(SupervisorJob())
-
-//    init {
-//        appScope.launch {//TODO уточнить насчёт в каком потоке запускается.
-//            val resultConfiguration = repositoryNetworkConfiguration.getResultConfiguration()
-//            if (resultConfiguration is Result.Success) {
-//                val configURLImage = resultConfiguration.data as ConfigURLImage
-//                _stateConfigURLImage.emit(StateConfigURLImage.Success(configURLImage))
-//            }
-//            if (resultConfiguration is Result.Error) {
-//                _stateConfigURLImage.emit(
-//                    StateConfigURLImage.Error(
-//                        resultConfiguration.message
-//                    )
-//                )
-//            }
-//        }
-//    }
 
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
-//        configURLImage = stateConfigURLImage.value as ConfigURLImage
     }
 
 
     companion object {
         lateinit var appContext: Context
-////        lateinit var configURLImage: ConfigURLImage
-//        fun getFirstPartURL(imageType: NetworkConfig.ImageType): String {
-//            return when (imageType) {
-//                NetworkConfig.ImageType.MOVIE_POSTER -> configURLImage.let {
-//                    it.baseURLImage + it.posterSizes[NetworkConfig.POSTER_SIZE_MEDIUM]
-//                }
-//                NetworkConfig.ImageType.MOVIE_BACKDROP -> configURLImage.let {
-//                    it.baseURLImage + it.backdropSizes[NetworkConfig.BACKDROP_SIZE_SMALL]
-//                }
-//                NetworkConfig.ImageType.ACTOR_PHOTO -> configURLImage.let {
-//                    it.baseURLImage + it.profileSizes[NetworkConfig.ACTOR_PHOTO_SIZE_SMALL]
-//                }
-//            }
-//        }
     }
 }

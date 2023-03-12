4f6cc83cb2e8206c910ac0a67bb9531d3d6eebb0
orlovae <79202875363@ya.ru>
2023-03-12 19:09:23 +0300

    refactoring

--- app/src/main/java/ru/alexandrorlov/moviescompose/screen/ScreenMovieList.kt
+++ app/src/main/java/ru/alexandrorlov/moviescompose/screen/ScreenMovieList.kt
@@ -3,18 +3,30 @@ package ru.alexandrorlov.moviescompose.screen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
+import ru.alexandrorlov.moviescompose.R
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ScreenMovieList(
     viewModel: ViewModelMovieList = viewModel(
@@ -23,6 +35,16 @@ fun ScreenMovieList(
     onMovieClick: (Int) -> Unit
 ) {
     val state = viewModel.state.collectAsState()
+    val isRefreshing = viewModel.isRefreshing.collectAsState()
+    val refreshScope = rememberCoroutineScope()
+
+    fun refresh() = refreshScope.launch {
+        viewModel.update()
+    }
+
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = isRefreshing.value,
+        onRefresh = ::refresh)
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -38,7 +60,9 @@ fun ScreenMovieList(
                 Box(modifier = Modifier
                     .fillMaxSize()) {
                     Text(
-                        text = "ERROR\n" + (state.value as StateMovieList.Error).message,
+                        text = stringResource(R.string.error) +
+                                System.lineSeparator() +
+                                (state.value as StateMovieList.Error).message,
                         modifier = Modifier
                             .fillMaxWidth(),
                         fontSize = 30.sp,
@@ -49,13 +73,23 @@ fun ScreenMovieList(
                 }
             }
             is StateMovieList.Success -> {
-                LazyColumn {
-                    items(items = (state.value as StateMovieList.Success).movieList) { item ->
-                        ComponentMovie(
-                            movie = item,
-                            onClick = onMovieClick
-                        )
+                Box(
+                    modifier = Modifier
+                        .padding()
+                        .pullRefresh(pullRefreshState)) {
+                    LazyColumn {
+                        items(items = (state.value as StateMovieList.Success).movieList) { item ->
+                            ComponentMovie(
+                                movie = item,
+                                onClick = onMovieClick
+                            )
+                        }
                     }
+                    PullRefreshIndicator(
+                        refreshing = isRefreshing.value,
+                        state = pullRefreshState,
+                        modifier = Modifier
+                            .align(Alignment.TopCenter))
                 }
             }
         }
